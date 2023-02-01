b58e1de2dc044e4af8b4faaad997dfc025e4bf99
eyO18 <106310176+Ahmad-busaeri@users.noreply.github.com>
2023-02-01 15:48:31 +0700

    up first

---
+++ Project_Uas/src/Login.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+
+
+
+
+public  class Login {
+    
+    
+    public static void play() throws Exception{
+        Scanner sc = new Scanner(System.in);
+        
+        
+        System.out.print("masukan id \t \t:");
+        String id = sc.nextLine();
+        System.out.print("masukan password \t:");
+        String password = sc.nextLine();
+        try {
+            if (password.equals(Tools.Getpass(id))) {
+                AdminMenu.setuser(id);
+                AdminMenu.Play();
+            
+        }
+        else{
+            System.out.println("Id dan Password tidak sesuai");}
+            MenuAwal.Play();
+        } catch (Exception e) {
+            System.out.println("ID anda tidak terdaftar");
+            
+        }
+        
+            
+        }
+        
+        
+        
+    }
+    
+
+  
+    
+
+    
+
+
